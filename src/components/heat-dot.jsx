a52690c3d9703b674668ce6f29dcbf64cc39ec09
FadeZhanger <dzhan96@uwo.ca>
2018-09-15 15:31:12 -0400

    Changed pixed count to 2

--- src/components/heat-dot.jsx
+++ src/components/heat-dot.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import {Tooltip} from 'antd';
 
 class Heatmap extends Component {
   static propTypes = {
@@ -15,11 +16,15 @@ class Heatmap extends Component {
     console.log('the props', this.props);
     const zoom = this.props.$geoService.getZoom();
     const style = {
-      width: `${5 * zoom}px`,
-      height: `${5 * zoom}px`,
+      width: `${2 * zoom}px`,
+      height: `${2 * zoom}px`,
     };
 
-    return <div className='heat' style={style} />;
+    return (
+      <Tooltip title='test'>
+        <div className='heat' style={style} onMouseEnter={() => console.log('test')} />
+      </Tooltip>
+    );
   }
 }
 
