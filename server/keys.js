d19a26eb0a9b2e8c44a4dcbc43e1251fda03152a
mishnaid <mishnaid@edu.uwaterloo.ca>
2018-09-15 13:29:54 -0400

    heatmap

--- server/keys.js
+++ server/keys.js
@@ -2,6 +2,6 @@ export default {
   google: {
     clientId: '723772852154-sj86kos84rri18kqia492ib7opa3nrr1.apps.googleusercontent.com',
     clientSecret: 'pEHKXrC4bU-HQHuwgWK-YPhO',
-    apiKey: 'AIzaSyCmfMYsDtZ_r3VNpNO5zZvgXnoIEbjjw5Y',
+    apiKey: 'AIzaSyD_6GO3pjBsRu0xROPb8iQnlP04_xh4idA',
   },
 };
--- src/components/map-component.jsx
+++ src/components/map-component.jsx
@@ -1,28 +1,36 @@
 import React, {Component} from 'react';
 import GoogleMapReact from 'google-map-react';
+import Heatmap from './heatmap';
 
-const GOOGLE_API_KEY = 'AIzaSyAAdmfYG0av3FYi5RV-jK_sZKm2wS84BII';
-
-
+const GOOGLE_API_KEY = 'AIzaSyD_6GO3pjBsRu0xROPb8iQnlP04_xh4idA';
 
 class Map extends Component {
 
   static defaultProps = {
     center: {
-      lat: 59.95,
-      lng: 30.33
+      lat: 43.4653563,
+      lng: -80.5414864
     },
-    zoom: 11
+    zoom: 14
   };
 
   render() {
     return(
-      <div style={{ height: '100vh', width: '100%' }}>
+      <div style={{ height: '100%', width: '100%' }}>
         <GoogleMapReact
           bootstrapURLKeys={{ key: GOOGLE_API_KEY }}
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
-        />
+        >
+          <Heatmap
+            lat='43.4653563'
+            lng='-80.5414864'
+          />
+          <Heatmap
+            lat='42.4653563'
+            lng='-81.5414864'
+          />
+        </GoogleMapReact>
 
       </div>
     );
