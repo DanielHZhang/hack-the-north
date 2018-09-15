8006022d3856c0122d0d31013eebdf27c23bb80e
FadeZhanger <dzhan96@uwo.ca>
2018-09-15 12:23:12 -0400

    Added google api key on server side

---
+++ server/keys.js
@@ -0,0 +1,7 @@
+export default {
+  google: {
+    clientId: '723772852154-sj86kos84rri18kqia492ib7opa3nrr1.apps.googleusercontent.com',
+    clientSecret: 'pEHKXrC4bU-HQHuwgWK-YPhO',
+    apiKey: 'AIzaSyCmfMYsDtZ_r3VNpNO5zZvgXnoIEbjjw5Y',
+  },
+};
--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,7 +1,32 @@
-import {Router} from 'express';
+import {
+  Router
+} from 'express';
+import request from 'request-promise';
+import keys from '../../keys';
 
 const router = Router();
 
+/**
+ * Get the latitude and longitude given a street address
+ */
+router.get('/coordinates', async (req, res) => {
+  try {
+    const {
+      address,
+    } = req.params;
+    // eslint-disable-next-line
+    const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${keys.google.apiKey}`;
+    const options = {
+      url: getAutocompleteQueryUrl(query),
+      method: 'GET',
+      json: true,
+    };
+    const response = await request(options);
+
+  } catch (error) {
+
+  }
+});
 
 
 export default router;
